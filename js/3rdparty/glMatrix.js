25bb616278b3fd4140e5ada220e180dc51d6625e
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2012-07-04 15:22:25 +0200

    Fixes #5

--- js/3rdparty/glMatrix.js
+++ js/3rdparty/glMatrix.js
@@ -1,20 +1,4 @@
-/* Copyright STIFTELSEN SINTEF 2012
- * 
- * This file is part of the Tinia Framework.
- * 
- * The Tinia Framework is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * The Tinia Framework is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with the Tinia Framework.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 
 // glMatrix v0.9.5
 glMatrixArrayType=typeof Float32Array!="undefined"?Float32Array:typeof WebGLFloatArray!="undefined"?WebGLFloatArray:Array;var vec3={};vec3.create=function(a){var b=new glMatrixArrayType(3);if(a){b[0]=a[0];b[1]=a[1];b[2]=a[2]}return b};vec3.set=function(a,b){b[0]=a[0];b[1]=a[1];b[2]=a[2];return b};vec3.add=function(a,b,c){if(!c||a==c){a[0]+=b[0];a[1]+=b[1];a[2]+=b[2];return a}c[0]=a[0]+b[0];c[1]=a[1]+b[1];c[2]=a[2]+b[2];return c};
