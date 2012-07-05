df26c35b3f412280caf8e9b4502442cc2f56a312
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2012-07-05 14:34:27 +0200

    Created basic unittests for the ExposedModelScripting

--- src/qtcontroller/scripting/ExposedModel.cpp
+++ src/qtcontroller/scripting/ExposedModel.cpp
@@ -6,7 +6,7 @@ namespace scripting {
 
 
 ExposedModel::ExposedModel(std::shared_ptr<tinia::model::ExposedModel> model, QObject *parent) :
-    QObject(parent)
+    QObject(parent), m_model(model)
 {
 }
 
@@ -20,16 +20,16 @@ void ExposedModel::updateElement(const QString &key, const QString &value)
     }
 
 
-    if (type == "double") {
+    if (type == std::string("double")) {
         updateElement(key, boost::lexical_cast<double>(value.toStdString()));
     }
-    else if(type=="int")  {
+    else if(type==std::string("integer"))  {
         updateElement(key, boost::lexical_cast<int>(value.toStdString()));
     }
-    else if(type=="bool") {
+    else if(type==std::string("bool")) {
         updateElement(key, boost::lexical_cast<bool>(value.toStdString()));
     }
-    else if(type=="string") {
+    else if(type==std::string("string")) {
         m_model->updateElement(key.toStdString(), value.toStdString());
     }
 
--- unittests/qtcontroller/ExposedModelScriptingTest.cpp
+++ unittests/qtcontroller/ExposedModelScriptingTest.cpp
@@ -1,3 +1,72 @@
 #include <boost/test/unit_test.hpp>
+#include <tinia/qtcontroller/scripting/ExposedModel.hpp>
+
+BOOST_AUTO_TEST_SUITE(ExposedModelScripting)
+
+namespace {
+struct ModelScriptingFixture {
+    std::shared_ptr<tinia::model::ExposedModel> model;
+    tinia::qtcontroller::scripting::ExposedModel scriptingModel;
+    ModelScriptingFixture()
+        : model(new tinia::model::ExposedModel()), scriptingModel(model)
+    {
+        ;
+    }
+};
+}
+
+BOOST_FIXTURE_TEST_CASE(UpdateWithoutScriptString, ModelScriptingFixture) {
+    model->addElement<std::string>("key", "value");
+    scriptingModel.updateElement("key", QString("newValue"));
+
+    std::string newValue;
+    model->getElementValue("key", newValue);
+    BOOST_CHECK_EQUAL("newValue", newValue);
+}
 
 
+BOOST_FIXTURE_TEST_CASE(UpdateWithoutScriptInt, ModelScriptingFixture) {
+    model->addElement<int>("key", 0);
+    scriptingModel.updateElement("key", 1);
+
+    int newValue;
+    model->getElementValue("key", newValue);
+    BOOST_CHECK_EQUAL(1, newValue);
+
+    // Also test string conversion:
+    scriptingModel.updateElement("key", QString("2"));
+    model->getElementValue("key", newValue);
+    BOOST_CHECK_EQUAL(2, newValue);
+}
+
+BOOST_FIXTURE_TEST_CASE(UpdateWithoutScriptDouble, ModelScriptingFixture) {
+    model->addElement<double>("key", 0.0);
+    scriptingModel.updateElement("key", 1.0);
+
+    double newValue;
+    model->getElementValue("key", newValue);
+    BOOST_CHECK_EQUAL(1, newValue);
+
+    // Also test string conversion:
+
+    scriptingModel.updateElement("key", QString("2"));
+    model->getElementValue("key", newValue);
+    BOOST_CHECK_EQUAL(2, newValue);
+}
+
+BOOST_FIXTURE_TEST_CASE(UpdateWithoutScriptBool, ModelScriptingFixture) {
+    model->addElement<bool>("key", false);
+    scriptingModel.updateElement("key", true);
+
+    bool newValue;
+    model->getElementValue("key", newValue);
+    BOOST_CHECK_EQUAL(true, newValue);
+
+    // Also test string conversion:
+    scriptingModel.updateElement("key", QString("0"));
+    model->getElementValue("key", newValue);
+    BOOST_CHECK_EQUAL(false, newValue);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
