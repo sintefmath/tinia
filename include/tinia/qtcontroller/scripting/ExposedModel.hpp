118a44ed594ca45c1bd9d236e81572aecb738053
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2012-07-05 12:33:11 +0200

    Made first iteration of exposed model.

---
+++ include/tinia/qtcontroller/scripting/ExposedModel.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <QObject>
+#include <tinia/model/ExposedModel.hpp>
+
+namespace tinia {
+namespace qtcontroller {
+namespace scripting {
+
+class ExposedModel : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ExposedModel(std::shared_ptr<tinia::model::ExposedModel> model, QObject *parent = 0);
+    
+signals:
+    
+public slots:
+    void updateElement(const QString& key, const QString& value);
+    void updateElement(const QString& key, int value);
+    void updateElement(const QString& key, double value);
+    void updateElement(const QString& key, bool value);
+
+
+private:
+    std::shared_ptr<tinia::model::ExposedModel> m_model;
+    
+};
+
+} // namespace scripting
+} // namespace qtcontroller
+} // namespace tinia
+
+
