fd096457352897e18bb7ebc3fd89e3968c20c496
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2012-07-06 15:17:48 +0200

    Exposed mouseevents to scripts.

--- unittests/qtcontroller/EventHandler.cpp
+++ unittests/qtcontroller/EventHandler.cpp
@@ -90,8 +90,85 @@ BOOST_AUTO_TEST_CASE(MethodsCalled) {
     BOOST_CHECK_EQUAL(1, moveCalled);
     BOOST_CHECK_EQUAL(1, releaseCalled);
     BOOST_CHECK_EQUAL(1, pressCalled);
+}
+
+BOOST_AUTO_TEST_CASE(EventObject) {
+    auto model = std::make_shared<tinia::model::ExposedModel>();
+
+    QString script =
+            "moveX = -1;\n"
+            "moveY = -1;\n"
+            "pressX = -1;\n"
+            "pressY = -1;\n"
+            "releaseX = -1\n"
+            "releaseY = -1\n"
+            "function MyEventObjectTest(params) {\n"
+            "   this.model = params.exposedModel;\n"
+            "}\n"
+            "MyEventObjectTest.prototype = {\n"
+            "   mouseMoveEvent: function(event) {\n"
+            "       moveX = event.x;\n"
+            "       moveY = event.y;\n"
+            "   },\n"
+            "   mousePressEvent: function(event) {\n"
+            "       pressX = event.x;\n"
+            "       pressY = event.y;\n"
+            "   },\n"
+            "   mouseReleaseEvent: function(event) {\n"
+            "       releaseX = event.x;\n"
+            "       releaseY = event.y;\n"
+            "   }\n"
+            "}\n";
+    auto& engine = tinia::qtcontroller::scripting::ScriptEngine::getInstance()->engine();
+
+    engine.evaluate(script);
+
+    BOOST_CHECK_EQUAL(-1, engine.evaluate("moveX").toNumber());
+    BOOST_CHECK_EQUAL(-1, engine.evaluate("moveY").toNumber());
+
+    BOOST_CHECK_EQUAL(-1, engine.evaluate("pressX").toNumber());
+    BOOST_CHECK_EQUAL(-1, engine.evaluate("pressY").toNumber());
+
+    BOOST_CHECK_EQUAL(-1, engine.evaluate("releaseX").toNumber());
+    BOOST_CHECK_EQUAL(-1, engine.evaluate("releaseY").toNumber());
+
+    tinia::qtcontroller::scripting::EventHandler handler("MyEventObjectTest",
+                                                         model);
+
+    QMouseEvent *e = new QMouseEvent(QEvent::MouseMove, QPoint(42,43), QPoint(0,0),
+                                     Qt::NoButton, Qt::NoButton, Qt::NoModifier);
+
+    handler.mouseMoveEvent(e);
+    BOOST_CHECK_EQUAL(42, engine.evaluate("moveX").toNumber());
+    BOOST_CHECK_EQUAL(43, engine.evaluate("moveY").toNumber());
+
+    BOOST_CHECK_EQUAL(-1, engine.evaluate("pressX").toNumber());
+    BOOST_CHECK_EQUAL(-1, engine.evaluate("pressY").toNumber());
+
+    BOOST_CHECK_EQUAL(-1, engine.evaluate("releaseX").toNumber());
+    BOOST_CHECK_EQUAL(-1, engine.evaluate("releaseY").toNumber());
 
 
+    handler.mousePressEvent(e);
+    BOOST_CHECK_EQUAL(42, engine.evaluate("moveX").toNumber());
+    BOOST_CHECK_EQUAL(43, engine.evaluate("moveY").toNumber());
+
+    BOOST_CHECK_EQUAL(42, engine.evaluate("pressX").toNumber());
+    BOOST_CHECK_EQUAL(43, engine.evaluate("pressY").toNumber());
+
+    BOOST_CHECK_EQUAL(-1, engine.evaluate("releaseX").toNumber());
+    BOOST_CHECK_EQUAL(-1, engine.evaluate("releaseY").toNumber());
+
+
+    handler.mouseReleaseEvent(e);
+    BOOST_CHECK_EQUAL(42, engine.evaluate("moveX").toNumber());
+    BOOST_CHECK_EQUAL(43, engine.evaluate("moveY").toNumber());
+
+    BOOST_CHECK_EQUAL(42, engine.evaluate("pressX").toNumber());
+    BOOST_CHECK_EQUAL(43, engine.evaluate("pressY").toNumber());
+
+    BOOST_CHECK_EQUAL(42, engine.evaluate("releaseX").toNumber());
+    BOOST_CHECK_EQUAL(43, engine.evaluate("releaseY").toNumber());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
