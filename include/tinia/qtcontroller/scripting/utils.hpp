a2cd98fa7d9883f3ea62422d6ceecf1c385c1727
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2012-07-06 10:29:57 +0200

    Added simple loading mechanism for glmatrix.

---
+++ include/tinia/qtcontroller/scripting/utils.hpp
@@ -0,0 +1,30 @@
+/* Copyright STIFTELSEN SINTEF 2012
+ *
+ * This file is part of the Tinia Framework.
+ *
+ * The Tinia Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Tinia Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Tinia Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include <QtScript>
+
+
+namespace tinia { namespace qtcontroller { namespace scripting {
+
+/** Adds the glMatrix javascript modules to the engine
+ * @return The QScriptValue of the evaluation.
+ */
+QScriptValue addGlMatrix(QScriptEngine& engine);
+
+}}}
