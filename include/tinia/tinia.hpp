cabc68876c3137c3b3d71a52fc9f72ff76b3cb04
Johan Seland <johan.seland@sintef.no>
2012-07-10 13:10:22 +0200

    Started adding convenience headers.

---
+++ include/tinia/tinia.hpp
@@ -0,0 +1,13 @@
+#pragma once
+/** @file tinia.hpp
+ * @ingroup ConvenienceHeaders
+ * Convenience header to include all of Tinia with one include statemes.
+ *
+ * Include this file if you want to use Tinia without bothering with getting the right
+ * includes in every file.
+ */
+
+#include "tinia/model/model.hpp"
+
+/** @namespace tinia All C++ functionality of Tinia is contained in the tinia namespace. */
+
