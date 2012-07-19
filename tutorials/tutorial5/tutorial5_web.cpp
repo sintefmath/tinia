7d365556b59d3963ef3b430121274345ed082f61
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2012-07-19 18:12:10 +0200

    Added tutorial5 web.

---
+++ tutorials/tutorial5/tutorial5_web.cpp
@@ -0,0 +1,37 @@
+/** [headers] */
+#include "tutorial5_job.hpp"
+#include <Qt>
+#include "tinia/trell/IPCGLJobController.hpp"
+#include <QFile>
+#include <string>
+#include <exception>
+/** [headers] */
+
+/** [main] */
+int main(int argc, char** argv) {
+    /** [job] */
+    tinia::tutorial5::TextureDrawer job;
+    /** [job] */
+
+    /** [controller] */
+    tinia::trell::IPCGLJobController controller;
+    /** [controller] */
+    //Q_INIT_RESOURCE( tutorial5 );
+    QFile viewerSourceLoc( ":/tutorial5.js" );
+    if( !viewerSourceLoc.open( (QIODevice::ReadOnly | QIODevice::Text) ) ) {
+        std::string err = std::string(viewerSourceLoc.errorString().toAscii() );
+        throw std::runtime_error( "Could not open file in QRC, aborting."  + err);
+    }
+
+    std::string viewerSource ( QString(viewerSourceLoc.readAll()).toAscii() );
+    controller.addScript( viewerSource );
+
+    /** [jobtocontroller] */
+    controller.setJob(&job);
+    /** [jobtocontroller] */
+
+    /** [run]*/
+    return controller.run(argc, argv);
+    /** [run]*/
+}
+/** [main] */
