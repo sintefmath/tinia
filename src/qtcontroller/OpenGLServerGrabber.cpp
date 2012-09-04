e3554119e06f14fb7714541505fed39eec29c424
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2012-09-04 16:06:46 +0200

    Fixed syntax error (missing} )

--- src/qtcontroller/OpenGLServerGrabber.cpp
+++ src/qtcontroller/OpenGLServerGrabber.cpp
@@ -10,7 +10,8 @@ namespace impl {
 
 OpenGLServerGrabber::OpenGLServerGrabber(tinia::jobcontroller::Job* job,
                                          QObject *parent) :
-    QObject(parent), m_glImageIsReady(false), m_job(job),
+    QObject(parent), m_glImageIsReady(false), m_job(job)
+{
     connect(this, SIGNAL(glImageReady()), this,
             SLOT(wakeListeners()));
 
