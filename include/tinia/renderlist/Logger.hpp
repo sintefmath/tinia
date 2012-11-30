ad8a05401a48fba4e6d50abc2fa27ca1f5fa1449
Chris <cdyk@localhost>
2012-11-30 10:18:03 +0100

    Removed debug logging when compiled in non-debug mode

--- include/tinia/renderlist/Logger.hpp
+++ include/tinia/renderlist/Logger.hpp
@@ -1,17 +1,17 @@
 /* Copyright STIFTELSEN SINTEF 2012
- * 
+ *
  * This file is part of the Tinia Framework.
- * 
+ *
  * The Tinia Framework is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The Tinia Framework is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with the Tinia Framework.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -30,9 +30,15 @@ static inline Logger getLogger( const std::string name )
     return name;
 }
 
+#ifdef DEBUG
 #define RL_LOG_TRACE(a,b) do { std::cerr << "[T] " << a << ": " << b << std::endl; } while(0)
 #define RL_LOG_INFO(a,b)  do { std::cerr << "[I] " << a << ": " << b << std::endl; } while(0)
 #define RL_LOG_DEBUG(a,b) do { std::cerr << "[D] " << a << ": " << b << std::endl; } while(0)
+#else
+#define RL_LOG_TRACE(a,b) do { } while(0)
+#define RL_LOG_INFO(a,b)  do { } while(0)
+#define RL_LOG_DEBUG(a,b) do { } while(0)
+#endif
 #define RL_LOG_WARN(a,b)  do { std::cerr << "[W] " << a << ": " << b << std::endl; } while(0)
 #define RL_LOG_ERROR(a,b) do { std::cerr << "[E] " << a << ": " << b << std::endl; } while(0)
 #define RL_LOG_FATAL(a,b) do { std::cerr << "[F] " << a << ": " << b << std::endl; } while(0)
