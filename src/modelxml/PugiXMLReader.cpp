dbe7f2d5c9160afa9a54aea13f1aca501ba4e826
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2013-05-23 19:00:33 +0200

    Now reading simple element state.

--- src/modelxml/PugiXMLReader.cpp
+++ src/modelxml/PugiXMLReader.cpp
@@ -19,6 +19,36 @@ void PugiXMLReader::readState(const std::string& documentString) {
     if (!result) {
         throw std::runtime_error("Could not parse XML document");
     }
+
+    // Find State
+    pugi::xml_node stateNode = document.select_single_node("*/State").node();
+    if (!stateNode) {
+        throw std::runtime_error("Could not find a State node in the document");
+    }
+
+    // Now we traverse
+    for (pugi::xml_node_iterator stateIt = stateNode.begin(); stateIt != stateNode.end();
+         ++stateIt)
+    {
+        // We need to get the name:
+        std::string key = stateIt->name();
+
+        tinia::model::StateSchemaElement schemaElement
+                = m_exposedModel->getStateSchemaElement(key);
+
+        // We can update everything except complex type and matrix easily
+        if (schemaElement.getXSDType() == "xsd:complexType") {
+            // We have a complex type
+        }
+        else if (schemaElement.getLength() == ElementData::MATRIX_LENGTH) {
+            // Matrix type
+        } else {
+            // All other types may be updated directly
+            m_exposedModel->updateElementFromString(key, stateIt->first_child().value());
+        }
+
+
+    }
 }
 
 }}}}
--- unittests/modelxml/PugiXMLReaderTest.cpp
+++ unittests/modelxml/PugiXMLReaderTest.cpp
@@ -27,4 +27,129 @@ BOOST_FIXTURE_TEST_CASE(ReadFaultyDocumentAssertThrow, PugiXMLReaderTestFixture)
 
 }
 
+BOOST_FIXTURE_TEST_CASE(ReadDocumentWithoutStateAssertThrow, PugiXMLReaderTestFixture) {
+    std::string incorrectDocument = "<?xml version=\"1.0\"?>"
+            "<someothertag />";
+
+    BOOST_CHECK_THROW(reader.readState(incorrectDocument), std::exception);
+}
+
+BOOST_FIXTURE_TEST_CASE(ReadCorrectDocumentAssertNoThrow, PugiXMLReaderTestFixture) {
+    // First we need to add a variable to the exposedmodel to make this correct
+    model->addElement("key", 0);
+
+    std::string correctDocument = "<?xml version=\"1.0\"?>"
+                                   "<ExposedModelUpdate xmlns=\"http://cloudviz.sintef.no/V1/model\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://cloudviz.sintef.no/V1/model/ExposedModelUpdateSchema.xsd\" revision=\"10456\">"
+                                   "    <StateSchema xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
+                                   "        <xsd:schema>"
+                                   "            <xsd:element name=\"State\">"
+                                   "                 <xsd:complexType>"
+                                   "                      <xsd:all>"
+                                   "                          <xsd:element name=\"key\" type=\"xsd:integer\" />"
+                                   "                      </xsd:all>"
+                                   "                  </xsd:complexType>"
+                                   "            </xsd:element>"
+                                   "        </xsd:schema>"
+                                   "   </StateSchema>"
+                                   "   <State>"
+                                   "       <key>0</key>"
+                                   "   </State>"
+                                   "   <GuiLayout>"
+                                   "   </GuiLayout>"
+                                   "</ExposedModelUpdate>";
+
+    BOOST_CHECK_NO_THROW(reader.readState(correctDocument));
+}
+
+BOOST_FIXTURE_TEST_CASE(UpdateInt, PugiXMLReaderTestFixture) {
+    // First we need to add a variable to the exposedmodel to make this correct
+    model->addElement("key", 0);
+
+    std::string correctDocument = "<?xml version=\"1.0\"?>"
+                                   "<ExposedModelUpdate xmlns=\"http://cloudviz.sintef.no/V1/model\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://cloudviz.sintef.no/V1/model/ExposedModelUpdateSchema.xsd\" revision=\"10456\">"
+                                   "    <StateSchema xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
+                                   "        <xsd:schema>"
+                                   "            <xsd:element name=\"State\">"
+                                   "                 <xsd:complexType>"
+                                   "                      <xsd:all>"
+                                   "                          <xsd:element name=\"key\" type=\"xsd:integer\" />"
+                                   "                      </xsd:all>"
+                                   "                  </xsd:complexType>"
+                                   "            </xsd:element>"
+                                   "        </xsd:schema>"
+                                   "   </StateSchema>"
+                                   "   <State>"
+                                   "       <key>1</key>"
+                                   "   </State>"
+                                   "   <GuiLayout>"
+                                   "   </GuiLayout>"
+                                   "</ExposedModelUpdate>";
+
+    BOOST_CHECK_NO_THROW(reader.readState(correctDocument));
+    BOOST_CHECK_EQUAL(1, model->getElementValue<int>("key"));
+}
+
+BOOST_FIXTURE_TEST_CASE(UpdateString, PugiXMLReaderTestFixture) {
+    // First we need to add a variable to the exposedmodel to make this correct
+    model->addElement("key", std::string("value"));
+
+    std::string correctDocument = "<?xml version=\"1.0\"?>"
+                                   "<ExposedModelUpdate xmlns=\"http://cloudviz.sintef.no/V1/model\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://cloudviz.sintef.no/V1/model/ExposedModelUpdateSchema.xsd\" revision=\"10456\">"
+                                   "    <StateSchema xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
+                                   "        <xsd:schema>"
+                                   "            <xsd:element name=\"State\">"
+                                   "                 <xsd:complexType>"
+                                   "                      <xsd:all>"
+                                   "                          <xsd:element name=\"key\" type=\"xsd:string\" />"
+                                   "                      </xsd:all>"
+                                   "                  </xsd:complexType>"
+                                   "            </xsd:element>"
+                                   "        </xsd:schema>"
+                                   "   </StateSchema>"
+                                   "   <State>"
+                                   "       <key>new value</key>"
+                                   "   </State>"
+                                   "   <GuiLayout>"
+                                   "   </GuiLayout>"
+                                   "</ExposedModelUpdate>";
+
+    BOOST_CHECK_NO_THROW(reader.readState(correctDocument));
+    BOOST_CHECK_EQUAL(std::string("new value"), model->getElementValue<std::string>("key"));
+}
+
+BOOST_FIXTURE_TEST_CASE(UpdateStringAndInt, PugiXMLReaderTestFixture) {
+    // First we need to add a variable to the exposedmodel to make this correct
+    model->addElement("stringKey", std::string("value"));
+    model->addElement("intKey", 1);
+    std::string correctDocument = "<?xml version=\"1.0\"?>"
+                                   "<ExposedModelUpdate xmlns=\"http://cloudviz.sintef.no/V1/model\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://cloudviz.sintef.no/V1/model/ExposedModelUpdateSchema.xsd\" revision=\"10456\">"
+                                   "    <StateSchema xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
+                                   "        <xsd:schema>"
+                                   "            <xsd:element name=\"State\">"
+                                   "                 <xsd:complexType>"
+                                   "                      <xsd:all>"
+                                   "                          <xsd:element name=\"stringKey\" type=\"xsd:string\" />"
+                                   "                           <xsd:element name=\"intKey\" type=\"int\" />"
+                                   "                      </xsd:all>"
+                                   "                  </xsd:complexType>"
+                                   "            </xsd:element>"
+                                   "        </xsd:schema>"
+                                   "   </StateSchema>"
+                                   "   <State>"
+                                   "       <stringKey>new value</stringKey>"
+                                   "       <intKey>4</intKey>"
+                                   "   </State>"
+                                   "   <GuiLayout>"
+                                   "   </GuiLayout>"
+                                   "</ExposedModelUpdate>";
+
+    BOOST_CHECK_NO_THROW(reader.readState(correctDocument));
+    BOOST_CHECK_EQUAL(std::string("new value"),
+                      model->getElementValue<std::string>("stringKey"));
+    BOOST_CHECK_EQUAL(4,
+                      model->getElementValue<int>("intKey"));
+}
+
+
+
 BOOST_AUTO_TEST_SUITE_END()
